54739c7a3264018d7ac43a190ef35eb464271541
Alex Bordens <abordens@tribune.com>
2015-01-15 11:25:05 -0600

    complete excercise 3

---
+++ lpthw/ex3.py
@@ -0,0 +1,33 @@
+print "I will now count my chickens."
+
+# Divide 30 by 6 and add 25
+print "Hens", 25.0 + 30.0 / 6.0
+# Multiply 25 by 3, then module 4 to find the remainder and subtract from 100
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+print "Now I will count the eggs:"
+
+# Modulo 4 and 2, divide 1 by 4 then do the remaining addition and subtraction
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# Add 3 plus 2, then subtract 7 from 5 and compare if those to values are equal
+print 3.0 + 2.0 < 5.0 - 7.0
+
+# Add 3 and 2
+print "What is 3 + 2?", 3.0 + 2.0
+# Subtract 7 from 5
+print "What is 5 - 7?", 5.0 - 7.0
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+# Determine is 5 is greater than -2 and print the boolean result
+print "Is it greater?", 5.0 > -2.0
+# Determine is 5 is greater than or equal to -2 and print the boolean result
+print "Is it greater or equal?", 5.0 >= -2.0
+# Determine is 5 is less than or equal to -2 and print the boolean result
+print "Is it less or equal?", 5.0 <= -2.0
+
