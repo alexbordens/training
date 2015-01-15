ea1e179f105a1c3d76fa2b8c9a8c7cdb45c721be
Alex Bordens <abordens@tribune.com>
2015-01-15 15:02:32 -0600

    complete excercise seven

---
+++ lpthw/ex7.py
@@ -0,0 +1,30 @@
+# print the string statement
+print "Mary had a little lamb."
+
+# print the string statment with the string formatter
+print "Its fleece was white as %s." % 'snow'
+
+# print the statement
+print "And everywhere that Mary went."
+
+# print ten periods
+print "." * 10 # what'd that do?
+
+# set each variable to a letter that will eventually spell Cheese Burger
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end. try removing it and see what happens
+# print Cheese Burger on one line with the comma or two lines without
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
