700d07fde2839997f77dee0c9e128793ac2f8aae
Alex Bordens <abordens@tribune.com>
2015-01-15 16:17:10 -0600

    complete excercise eight

---
+++ lpthw/ex8.py
@@ -0,0 +1,22 @@
+# create a variable of string formatters
+formatter = "%r %r %r %r"
+
+# print all four numbers
+print formatter % (1, 2, 3, 4)
+
+# print the four strings
+print formatter % ("one", "two", "three", "four")
+
+# print the boolean as string
+print formatter % (True, False, False, True)
+
+# print the formatters over and over 
+print formatter % (formatter, formatter, formatter, formatter)
+
+# print each string with single quotes
+print formatter % (
+	'I had this thing.,
+	'That you could type up right.',
+	"But it didn't sing.",
+	'So I said goodnight.'
+	)
