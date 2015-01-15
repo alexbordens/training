39c188214a508218b9f3a35fe7eefe7c0ca5748f
Alex Bordens <abordens@tribune.com>
2015-01-15 14:49:51 -0600

    complete excercise six for real

--- lpthw/ex6.py
+++ lpthw/ex6.py
@@ -10,6 +10,7 @@ do_not = "don't"
 # setting the string
 y = "Those who know %s and those who %s." % (binary, do_not)
 
+# print the x and y strings
 print x
 print y
 
@@ -18,12 +19,18 @@ print "I said: %r." % x
 # print the statement and place the string assigned to y inside
 print "I also said: '%s'." % y
 
+# set the variable to boolean
 hilarious = False
+
+# set the variable to a string with a string formatter
 joke_evaluation = "Isn't that joke so funny?! %r"
 
+# print the string with hilarious inside 
 print joke_evaluation % hilarious
 
+# assign string variables
 w = "This is the left side of..."
 e = "a string with a right side."
 
+# print the two strings
 print w + e
